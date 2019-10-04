c8c71bb1bb54a5e68c27cc26fe1bbd62e5e34916
tempate <ddiazq28@gmail.com>
2019-10-05 00:37:37 +0200

    TTs and Move ordering

--- src/search.cpp
+++ src/search.cpp
@@ -1,16 +1,22 @@
-#include <vector>
-#include <cassert>
-#include <limits>
 #include <algorithm>
+#include <cassert>
 #include <ctime>
+#include <limits>
+#include <vector>
 
 #include "board.h"
+#include "hashtables.h"
 #include "moves.h"
-#include "uai.h"
 #include "search.h"
+#include "uai.h"
 
-int alphabeta(const Board &board, std::vector<Move> &pv, const int depth, int alpha, const int beta);
+enum { EXACT, LOWER_BOUND, UPPER_BOUND };
 
+int alphabeta(const Board &board, std::vector<Move> &pv, const int depth,
+              int alpha, int beta);
+
+void sort(std::vector<Move> &moves);
+void insertionSort(std::vector<Move> &moves);
 void timeManagement(const Board &board);
 
 clock_t start;
@@ -41,22 +47,48 @@ Move search(const Board &board) {
     return pv.front();
 }
 
-int alphabeta(const Board &board, std::vector<Move> &pv, const int depth, int alpha, const int beta) {
-	if (settings.stop)
-		return 0;
+int alphabeta(const Board &board, std::vector<Move> &pv, const int depth,
+              int alpha, int beta) {
+    if (settings.stop)
+        return 0;
 
-	if (settings.movetime && settings.nodes % 4096 == 0 && clock() - start > settings.movetime) {
+    if (settings.movetime && settings.nodes % 4096 == 0 &&
+        clock() - start > settings.movetime) {
         settings.stop = true;
         return 0;
     }
-    
+
     settings.nodes++;
-    
+
     if (depth == 0)
         return board.eval();
 
+    Entry entry = tt.get(board.key);
+
+    // Transposition Table
+    if (entry.key == board.key && entry.depth == depth) {
+
+#ifdef DEBUG
+        ++stats.ttHits;
+#endif
+
+        switch (entry.flag) {
+        case LOWER_BOUND:
+            alpha = std::max(alpha, entry.score);
+            break;
+        case UPPER_BOUND:
+            beta = std::min(beta, entry.score);
+            break;
+        case EXACT:
+            return entry.score;
+        }
+
+        if (alpha >= beta)
+            return entry.score;
+    }
+
     int bestScore = std::numeric_limits<int>::min();
-    
+
     std::vector<Move> childPV;
     std::vector<Move> moves = board.genMoves();
 
@@ -64,27 +96,35 @@ int alphabeta(const Board &board, std::vector<Move> &pv, const int depth, int al
         if (board.empty.popCount() == 0)
             return board.score();
 
+        if (board.pieces[board.turn].popCount() == 0)
+            return -MATE_SCORE;
+
+        assert(false);
+
         // If the board is not full the player passes its turn
-        moves.push_back(Move{});
+        // moves.emplace_back();
+    } else {
+        sort(moves);
     }
+
+    const int prevAlpha = alpha;
 
     for (Move move : moves) {
         Board copy = board;
         copy.make(move);
+        copy.genKey(FANCY_TT);
 
         const int score = -alphabeta(copy, childPV, depth - 1, -beta, -alpha);
 
         if (score > bestScore) {
             bestScore = score;
 
+            pv.assign(1, move);
+            pv.insert(pv.begin() + 1, childPV.begin(), childPV.end());
+
             if (bestScore > alpha) {
                 alpha = bestScore;
 
-                pv.assign(1, move);
-
-                if (childPV.size() > 0)
-                    pv.insert(pv.begin() + 1, childPV.begin(), childPV.end());
-                
                 if (alpha >= beta)
                     break;
             }
@@ -93,26 +133,66 @@ int alphabeta(const Board &board, std::vector<Move> &pv, const int depth, int al
 
     assert(bestScore > std::numeric_limits<int>::min());
 
+    int flag = EXACT;
+
+    if (bestScore <= prevAlpha)
+        flag = UPPER_BOUND;
+    else if (bestScore >= beta)
+        flag = LOWER_BOUND;
+
+    // Always replace the entry for the TT
+    assert(pv.size() > 0);
+    tt.add(board.key, pv[0], bestScore, depth, flag);
+
     return bestScore;
 }
 
+void sort(std::vector<Move> &moves) {
+    for (Move move : moves) {
+        switch (move.type) {
+        case NULL_MOVE:
+            move.score = -100;
+        case SINGLE:
+            move.score = 100;
+        case DOUBLE:
+            move.score = 0;
+        }
+    }
+
+    insertionSort(moves);
+}
+
+void insertionSort(std::vector<Move> &moves) {
+    for (int i = 1; i < moves.size(); ++i) {
+        int j = i - 1;
+
+        while (j >= 0 && moves[j].score < moves[i].score) {
+            moves[j + 1] = moves[j];
+            --j;
+        }
+
+        moves[j + 1] = moves[i];
+    }
+}
+
 void timeManagement(const Board &board) {
-	if (!settings.movetime) {
-		clock_t remaining, increment;
+    if (!settings.movetime) {
+        clock_t remaining, increment;
 
-		if (board.turn == BLUE) {
-			remaining = settings.wtime;
-			increment = settings.winc;
-		} else {
-			remaining = settings.btime;
-			increment = settings.binc;
-		}
+        if (board.turn == BLUE) {
+            remaining = settings.wtime;
+            increment = settings.winc;
+        } else {
+            remaining = settings.btime;
+            increment = settings.binc;
+        }
 
-		if (remaining || increment) {
-			const clock_t timeToMove = std::min(remaining >> 2, (remaining >> 5) + increment) - 20;
-			settings.movetime = (timeToMove * CLOCKS_PER_SEC) / 1000;
-		}
-	} else {
-		settings.movetime *= CLOCKS_PER_SEC / 1000;
-	}
+        if (remaining || increment) {
+            const clock_t timeToMove =
+                std::min(remaining >> 2, (remaining >> 5) + increment) - 20;
+            settings.movetime = (timeToMove * CLOCKS_PER_SEC) / 1000;
+        }
+    } else {
+        settings.movetime *= CLOCKS_PER_SEC / 1000;
+    }
 }

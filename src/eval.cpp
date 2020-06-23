b744878037afa78698f095310f9a4fc7f1d2aac8
Tempate <ddiazq28@gmail.com>
2020-06-23 18:48:47 +0200

    Safety patch

--- src/eval.cpp
+++ src/eval.cpp
@@ -4,66 +4,34 @@
 #include "eval.hpp"
 #include "tuner/tuner.hpp"
 
-#include <tuple>
-
-int countHoles(const Board &board, const int side);
-int psqtScore(const Board &board, const int side);
-int holesScore(const Board &board, const int side);
+int psqt_score(const Board &board, const int side);
+int holes_score(const Board &board, const int side);
+int safety_score(const Board &board, const int side);
 
 const int stone_value = 100;
 
 int eval(const Board &board) {
     const int material = board.stones[board.turn].popCount() - board.stones[board.turn ^ 1].popCount();
-    
-    #if TUNING
-        const int psqt = tuner.tunePsqtScore(board, board.turn) - tuner.tunePsqtScore(board, board.turn ^ 1);
-    #else
-        const int psqt = psqtScore(board, board.turn) - psqtScore(board, board.turn ^ 1);
-    #endif
 
-    const int holes = holesScore(board, board.turn) - holesScore(board, board.turn ^ 1);
+    const int psqt = psqt_score(board, board.turn) - psqt_score(board, board.turn ^ 1);
+    const int holes = holes_score(board, board.turn) - holes_score(board, board.turn ^ 1);
+    const int safety = safety_score(board, board.turn) - safety_score(board, board.turn ^ 1);
 
-    return stone_value * (material + 10 * board.turn) + psqt + holes;
+    return stone_value * (material + 10 * board.turn) + psqt + holes + safety;
 }
 
-int psqtScore(const Board &board, const int side) {
-    /*
-    static const std::array<int, 49> psqt_normal = {
-        90, 70, 70, 70, 70, 70, 90,
-        70,  0,  0,  0,  0,  0, 70,
-        70,  0,  0,  0,  0,  0, 70,
-        70,  0,  0,  0,  0,  0, 70,
-        70,  0,  0,  0,  0,  0, 70,
-        70,  0,  0,  0,  0,  0, 70,
-        90, 70, 70, 70, 70, 70, 90
+int psqt_score(const Board &board, const int side) {
+    static const std::vector<std::pair<Bitboard, int>> psqt_normal = {
+        std::make_pair(Bitboard{(uint64_t) 0b1000001000000000000000000000000000000000001000001}, 90),
+        std::make_pair(Bitboard{(uint64_t) 0b0111110100000110000011000001100000110000010111110}, 70)
     };
 
-    static const std::array<int, 49> psqt_endgame = {
-        45, 35, 35, 35, 35, 35, 45,
-        35,  0,  0,  0,  0,  0, 35,
-        35,  0,  0,  0,  0,  0, 35,
-        35,  0,  0,  0,  0,  0, 35,
-        35,  0,  0,  0,  0,  0, 35,
-        35,  0,  0,  0,  0,  0, 35,
-        45, 35, 35, 35, 35, 35, 45
+    static const std::vector<std::pair<Bitboard, int>> psqt_endgame = {
+        std::make_pair(Bitboard{(uint64_t) 0b1000001000000000000000000000000000000000001000001}, 30),
+        std::make_pair(Bitboard{(uint64_t) 0b0111110100000110000011000001100000110000010111110}, 10)
     };
 
     const auto psqt = (board.empty.popCount() > 10) ? psqt_normal : psqt_endgame;
-    */
-
-    static const std::array<std::pair<Bitboard, int>, 3> psqt_normal = {
-        std::make_pair(Bitboard{(uint64_t) 0b1000001000000000000000000000000000000000001000001}, 90),
-        std::make_pair(Bitboard{(uint64_t) 0b0111110100000110000011000001100000110000010111110}, 70),
-        std::make_pair(Bitboard{(uint64_t) 0b0000000000000000000000001000000000000000000000000}, 0)
-    };
-
-    static const std::array<std::pair<Bitboard, int>, 3> psqt_endgame = {
-        std::make_pair(Bitboard{(uint64_t) 0b1000001000000000000000000000000000000000001000001}, 30),
-        std::make_pair(Bitboard{(uint64_t) 0b0111110100000110000011000001100000110000010111110}, 10),
-        std::make_pair(Bitboard{(uint64_t) 0b1100011110001100000000000000000000011000111100011}, 0)
-    };
-
-    const auto psqt = (board.empty.popCount() > 5) ? psqt_normal : psqt_endgame;
 
     int score = 0;
 
@@ -73,7 +41,7 @@ int psqtScore(const Board &board, const int side) {
     return score;
 }
 
-int holesScore(const Board &board, const int side) {
+int holes_score(const Board &board, const int side) {
     static const std::array<int, 8> penalties = {0, 25, 50, 100, 150, 250, 350, 400};
 
     const Bitboard bb = board.stones[side].singles() & 
@@ -89,3 +57,48 @@ int holesScore(const Board &board, const int side) {
 
     return score;
 }
+
+int safety_score(const Board &board, const int side) {
+    if (board.empty.popCount() < 10)
+        return 0;
+
+    const Bitboard singles = board.stones[side].singles();
+    
+    const int weaknesses = (singles & board.empty).popCount();
+    const int penalty = -25;
+
+    const int safeties = (singles & board.stones[side]).popCount();
+    const int bonus = 10;
+
+    return weaknesses * penalty + safeties * bonus;
+}
+
+int index_singles(const Board &board, const int sqr) {
+    assert(sqr % 7 > 0 && sqr % 7 < 6);
+    assert(sqr / 7 > 0 && sqr / 7 < 6);
+
+    Bitboard singles = singlesLookup[sqr];
+    const Bitboard singles_white = singles & board.stones[WHITE];
+    const Bitboard singles_black = singles & board.stones[BLACK];
+
+    int index = 0;
+
+    for (int i = 0; i < singles.popCount(); i++) {
+        const Bitboard bb = singles.lsbBB();
+        singles.unsetLSB();
+
+        int value;
+
+        if (bb & singles_white)
+            value = 1;
+        else if (bb & singles_black)
+            value = 2;
+        else
+            value = 0;
+
+        index *= 3;
+        index += value;
+    }
+
+    return index;
+} 

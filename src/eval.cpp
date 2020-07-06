f6f7ecb8bf88d4494aa2cc40410aa50f46749426
Tempate <ddiazq28@gmail.com>
2020-07-07 00:47:07 +0200

    Give bonuses and penalties for different pockets

--- src/eval.cpp
+++ src/eval.cpp
@@ -7,8 +7,9 @@
 int psqt_score(const Board &board, const int side);
 int holes_score(const Board &board, const int side);
 int safety_score(const Board &board, const int side);
-//int density_score(const Board &board);
+int pocket_score(const Board &board, const int side);
 
+//int density_score(const Board &board);
 //std::vector<Bitboard> gen_regions(const int size);
 
 
@@ -18,16 +19,18 @@ int eval(const Board &board) {
     int score = 0;
 
     const int material = board.stones[board.turn].popCount() - board.stones[board.turn ^ 1].popCount();
+    
+    score += material * stone_value;
 
-    score += material;
-    score += 10 * board.turn;
-    score *= stone_value;
+    const int komi = 7 * stone_value;
+    score += komi * board.turn;
     
     score += psqt_score(board, board.turn) - psqt_score(board, board.turn ^ 1);
     score += holes_score(board, board.turn) - holes_score(board, board.turn ^ 1);
     score += safety_score(board, board.turn) - safety_score(board, board.turn ^ 1);
+    score += pocket_score(board, board.turn) - pocket_score(board, board.turn ^ 1);
 
-    const int deviation = 75;
+    int deviation = 75;
 
     if (material > 0)
         score += deviation;
@@ -38,15 +41,15 @@ int eval(const Board &board) {
 }
 
 int psqt_score(const Board &board, const int side) {
-    static const std::vector<std::pair<Bitboard, int>> psqt_normal = {
-        std::make_pair(Bitboard{(uint64_t) 0b1000001000000000000000000000000000000000001000001}, 90),
-        std::make_pair(Bitboard{(uint64_t) 0b0111110100000110000011000001100000110000010111110}, 70)
-    };
+    static const std::vector<std::pair<Bitboard, int>> psqt_normal {{
+        {Bitboard{(uint64_t) 0b1000001000000000000000000000000000000000001000001}, 90},
+        {Bitboard{(uint64_t) 0b0111110100000110000011000001100000110000010111110}, 70}
+    }};
 
-    static const std::vector<std::pair<Bitboard, int>> psqt_endgame = {
-        std::make_pair(Bitboard{(uint64_t) 0b1000001000000000000000000000000000000000001000001}, 30),
-        std::make_pair(Bitboard{(uint64_t) 0b0111110100000110000011000001100000110000010111110}, 10)
-    };
+    static const std::vector<std::pair<Bitboard, int>> psqt_endgame {{
+        {Bitboard{(uint64_t) 0b1000001000000000000000000000000000000000001000001}, 30},
+        {Bitboard{(uint64_t) 0b0111110100000110000011000001100000110000010111110}, 10}
+    }};
 
     const auto psqt = (board.empty.popCount() > 10) ? psqt_normal : psqt_endgame;
 
@@ -90,6 +93,49 @@ int safety_score(const Board &board, const int side) {
     return weaknesses * penalty + safeties * bonus;
 }
 
+int pocket_score(const Board &board, const int side) {
+    using PocketArray = std::array<Bitboard, 4>;
+
+    static const PocketArray corner_pockets {
+        Bitboard{(uint64_t) 0b1100000110000000000000000000000000000000000000000},
+        Bitboard{(uint64_t) 0b0000011000001100000000000000000000000000000000000},
+        Bitboard{(uint64_t) 0b0000000000000000000000000000000000011000001100000},
+        Bitboard{(uint64_t) 0b0000000000000000000000000000000000000000110000011}
+    };
+    
+    static const PocketArray corner_suburbs {
+        Bitboard{(uint64_t) 0b0010000001000011100000000000000000000000000000000},
+        Bitboard{(uint64_t) 0b0000100000010000001110000000000000000000000000000},
+        Bitboard{(uint64_t) 0b0000000000000000000000000000111000000100000010000},
+        Bitboard{(uint64_t) 0b0000000000000000000000000000000011100001000000100},
+    };
+
+    static const PocketArray center_pockets {
+        Bitboard{(uint64_t) 0b0000000000000000000000001100000110000000000000000},
+        Bitboard{(uint64_t) 0b0000000000000000011000001100000000000000000000000},
+        Bitboard{(uint64_t) 0b0000000000000000000000011000001100000000000000000},
+        Bitboard{(uint64_t) 0b0000000000000000110000011000000000000000000000000}
+    };
+
+    static const int corner_stone_bonus = 10;
+    static const std::array<int, 5> corner_suburb_stone_bonus = {0, 0, 2, 5, 15};
+    static const int center_stone_penalty = -5;
+
+    int score = 0;
+
+    for (int i = 0; i < 4; i++) {
+        const int corner_stones = (board.stones[side] & corner_pockets[i]).popCount();
+        const int suburb_stones = (board.stones[side] & corner_suburbs[i]).popCount();
+        const int center_stones = (board.stones[side] & center_pockets[i]).popCount();
+
+        score += corner_stones * corner_stone_bonus;
+        score += corner_suburb_stone_bonus[corner_stones] * suburb_stones;
+        score += center_stones * center_stone_penalty;
+    }
+
+    return score;
+}
+
 /*
 std::vector<Bitboard> gen_regions(const int size) {
     static const std::array<Bitboard, 7> bases = {
@@ -110,7 +156,7 @@ std::vector<Bitboard> gen_regions(const int size) {
     regions.assign(1, bases[size - 1]);
 
     for (int i = 0; i < count; i++) {
-        const int shift = 1 + static_cast<int>(i % 7 == repetitions);
+        const int shift = 1 + (size - 1) * static_cast<int>(i % 7 == repetitions);
         regions.push_back(regions[i] << shift);
     }
 
@@ -118,12 +164,7 @@ std::vector<Bitboard> gen_regions(const int size) {
 }
 
 int density_score(const Board &board) {
-    static const std::vector<Bitboard> regions = {
-        Bitboard{(uint64_t) 0b1110000111000011100000000000000000000000000000000},
-        Bitboard{(uint64_t) 0b0000111000011100001110000000000000000000000000000},
-        Bitboard{(uint64_t) 0b0000000000000000000000000000000011100001110000111},
-        Bitboard{(uint64_t) 0b0000000000000000000000000000111000011100001110000}
-    };
+    static const std::vector<Bitboard> regions = gen_regions(2);
 
     int score = 0;
 
@@ -132,15 +173,20 @@ int density_score(const Board &board) {
         const int other = (board.stones[board.turn ^ 1] & region).popCount();
         const int empty = mine - other;
 
-        if (mine > other)
+        if (empty != 3)
+            continue;
+
+        if (mine == 1)
             score += -10;
-        else if (mine < other)
+        else if (other == 1)
             score -= -10;        
     }
 
     return score;
 }
+*/
 
+/*
 int index_singles(const Board &board, const int sqr) {
     assert(sqr % 7 > 0 && sqr % 7 < 6);
     assert(sqr / 7 > 0 && sqr / 7 < 6);
